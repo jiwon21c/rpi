dd0dfae22399e824b176cb42d8dd73575e34f278
John Lee <jiwon21c@gmail.com>
2015-03-31 18:32:08 -0400

    wiringPi folder with sources and headers files is included to solve compiling and linking errors.

---
+++ Rpi_SuperKit/07_Beep/Default/beep.d
@@ -0,0 +1,4 @@
+beep.d: ../beep.c \
+ /home/jiwon21c/workspace/rpi/wiringPi/wiringPi/wiringPi.h
+
+/home/jiwon21c/workspace/rpi/wiringPi/wiringPi/wiringPi.h:
---
+++ Rpi_SuperKit/07_Beep/Default/sn3218.d
@@ -0,0 +1,9 @@
+sn3218.d: ../sn3218.c \
+ /home/jiwon21c/workspace/rpi/wiringPi/wiringPi/wiringPi.h \
+ /home/jiwon21c/workspace/rpi/wiringPi/wiringPi/wiringPiI2C.h ../sn3218.h
+
+/home/jiwon21c/workspace/rpi/wiringPi/wiringPi/wiringPi.h:
+
+/home/jiwon21c/workspace/rpi/wiringPi/wiringPi/wiringPiI2C.h:
+
+../sn3218.h:
